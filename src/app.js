1ba0d74b6edd5702fcaccd1899f070f461195870
Manish Juriani <manish_juriani@persistent.co.in>
2019-08-30 21:31:18 +0530

    made 3 charts

--- src/app.js
+++ src/app.js
@@ -1,66 +1,140 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LineChart from 'react-linechart'
+import {Line} from 'react-chartjs-2'
 
 export default class App extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            data: []
+            count:50,
+            data: {},
+            datasets: [],
+            xValues:[]
+        }
+        this.handleFlaskApi = this.handleFlaskApi.bind(this)
+    }
+
+    handleFlaskApi(){
+        const count = 15;
+        const borderColor = "green"
+        fetch('http://localhost:5000/ages/'+count)
+        .then(res => res.json())
+        .then((receivedData) => {
+            let yValues = []
+            let label = "Age"
+
+            for(let i=0;i<count;i++){
+                // xValues.push(receivedData[i].x)
+                yValues.push(receivedData[i].y)
+            }
+            this.addGraphData(yValues,borderColor,label)
+        }).catch(console.log)
+    }
+
+    addGraphData(yValues,borderColor,label){
+        let rawData = {
+            label:label,
+            fill:false,
+            borderColor:borderColor,
+            data:yValues
+        }
+        let datasets = this.state.datasets
+        datasets.push(rawData)
+        
+        let data = {
+            labels:this.setHorizontalValues(),
+            datasets:datasets
+        }
+        // console.log(datasets)
+        this.setState(()=>({
+            data:data,
+            datasets:datasets
+        }))
+    }
+
+    setHorizontalValues(){
+        let xValues = this.state.xValues;
+        if(xValues.length>=1){
+            return xValues
+        }else{
+            for(let i=0;i<this.state.count;i++){
+                xValues.push(i)
+            }
+            this.setState(()=>{xValues:xValues})
+            return xValues
         }
     }
 
     componentWillMount(){
-        const count = 3;
-        fetch('http://localhost:5000/ages/'+count)
+
+        const labels=[]
+
+        // Call to first api 
+        this.handleFlaskApi()
+
+        fetch("https://jsonplaceholder.typicode.com/users")
         .then(res => res.json())
-        .then((receivedData) => {
-            let data = []
-            let points = []
-            let input = {}
-            let color = receivedData[0].color
-
-            for(let i=0;i<count;i++){
-                points.push({x:receivedData[0].points[i].x,y:receivedData[0].points[i].y})
+        .then((receivedData)=>{
+            let size = receivedData.length
+            let longitudeValues = []
+            let latitudeValues = []
+            let borderColor = "purple"
+            let label = "Longitudes"
+            for(let i=0;i<size;i++){
+                longitudeValues.push(Math.floor(receivedData[i].address.geo.lng));
+                latitudeValues.push(Math.floor(receivedData[i].address.geo.lat));            
             }
-            
-            input = {
-                color: color,
-                points: points
-            }
-            data.push(input)  
-            console.log(data)
-
-            let string_data = JSON.stringify(data)
-            console.log(string_data)
-            this.setState(()=>({data:string_data}))
-            // console.log(this.state.data)
+            this.addGraphData(longitudeValues,borderColor,label)
+            this.addGraphData(latitudeValues,"cyan","Latitudes")
         }).catch(console.log)
-    }
 
+        const data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            labels: [1, "febr", 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+              {
+                label: 'My First dataset',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40]
+              },
+              {
+                label: 'Age',
+                fill: false,
+                borderColor:"yellow",
+                data: [50, 9, 8, 181, 67, 75]
+              }
+            ]
+          };
+        // this.setState(()=>({data:data}))
+    }
+    
         
     render(){
-
-        const data = [
-            {									
-                color: "orange", 
-                points: [{x: 1, y: 2}, {x: 3, y: 5}, {x: 7, y: -3}] 
-            }
-        ];
-
-
         return (
             <div>
                 <div>
                     <h1>Charts here</h1>
                 </div>
-                <div className="App">
-                    <h1>My First LineChart =={this.state.data}==</h1>
-                    <LineChart 
-                        width={600}
-                        height={400}
-                        data={data}
-                    />
+                <div className="App" style={{ height: 800 }}>
+                    <Line ref="chart" data={this.state.data} />
+                    {/* <Line ref="chart" data={data} /> */}
                 </div>				
             </div>
         );
